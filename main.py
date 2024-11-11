1572ae54863a6c56c58f6fe21bacd8e7f0a79a7a
marconesdtu <159428141+marconesdtu@users.noreply.github.com>
2024-11-11 16:41:55 +0100

    first try

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Define the file paths
+file1 = '/home/marco/DTU/CTfDS/project/medicine_details.csv'
+
+# Read the Excel files
+df1 = pd.read_csv(file1)
+
+# Print the dataframes to verify
+# print("Data from medicine_details.csv:")
+# print(df1)  
+
