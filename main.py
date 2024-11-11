f60cf2dc5ff92eefebb9ab3599331797ecbc8824
marconesdtu <s233557@dtu.dk>
2024-11-11 17:17:18 +0100

    matrixcomponents

--- main.py
+++ main.py
@@ -2,14 +2,30 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 # Define the file paths
-file1 = '/home/marco/DTU/CTfDS/project/medicine_details.csv'
+file1 = '/home/marco/DTU/CTfDS/project/computationaltoolsDS/medicine_details.csv'
 
 # Read the Excel files
-df1 = pd.read_csv(file1)
+data = pd.read_csv(file1)
 
 # Print the dataframes to verify
-# print("Data from medicine_details.csv:")
-# print(df1)  
+# print("data from medicine_details.csv:")
+# print(data)  
 
+
+# Print the dataframe to verify the new feature
+# print("data with ratings:")
+# print(data)
+
+# Step 1: Split the Composition column by " + " to get a list of components for each observation
+data['Composition'] = data['Composition'].str.split(' + ')
+
+# Step 2: Apply One-Hot Encoding
+# This creates a dataFrame where each unique component is a column with 1 or 0 indicating presence in each row
+one_hot_data = data['Composition'].str.join('|').str.get_dummies()
+
+# Step 3: Concatenate the one-hot encoded columns back with the original dataFrame (if needed)
+data = pd.concat([data, one_hot_data], axis=1)
+
+# Display the resulting dataFrame
+print(data.columns)
